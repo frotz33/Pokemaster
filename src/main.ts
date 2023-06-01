3a4dd06e98633211c2cea88bc5ecb33ee1c1a26b
frotz33 <krystian.stelmach3@gmail.com>
2023-06-01 19:53:36 +0200

    Updates

--- src/main.ts
+++
@@ -1,9 +0,0 @@
-import { GameRules } from './components/game-rules/GameRules';
-
-const answerTime = 15;
-const gameTime = 120;
-
-const initializePokemaster = async () => {
-  GameRules.init({ gameTime: gameTime, answerTime: answerTime });
-};
-initializePokemaster();
--- src/utils/ElementGetter.ts
+++
@@ -1,7 +0,0 @@
-export class ElementGetter {
-  static get = (selector: string): HTMLElement => {
-    const element = document.querySelector<HTMLElement>(selector);
-    if (!element) throw new Error(`There is${selector} missing`);
-    return element;
-  };
-}
